2b092d9ea40e73ecd77acc1550b35212a4588465
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 00:05:04 +0000

    Adding tarfile member sanitization to extractall()

--- postcalib/core.py
+++ postcalib/core.py
@@ -142,7 +142,26 @@ java -Xmx4000M -classpath "{0}:$CLASSPATH" uk.ac.starlink.ttools.Stilts "$@"
         funpack_src = os.path.join(externdir, 'cfitsio')
         import tarfile
         with tarfile.open(funpack_tmp) as tar:
-            tar.extractall(path=externdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=externdir)
         logger.warning("try compiling funpack")
         import subprocess
         try:
